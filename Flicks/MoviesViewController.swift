79cb4462ab30eeb106416ed4fdbec98c5c7ad59c
Mhatre, Aniket <Aniket_Mhatre@intuit.com>
2017-04-02 08:36:47 -0700

    tableView created

---
+++ Flicks/MoviesViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MoviesViewController.swift
+//  Flicks
+//
+//  Created by Mhatre, Aniket on 4/2/17.
+//  Copyright © 2017 Mhatre, Aniket. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var moviesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        moviesTableView.delegate = self
+        moviesTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        
+        cell.textLabel?.text = "row \(indexPath)"
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
